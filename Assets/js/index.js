489a6f2e97838bca716ee3bbe26066b3eb227b06
CPC2-2 <cmtorreno@gmail.com>
2024-09-30 14:52:30 +0200

    updated some changed on code

---
+++ Assets/js/index.js
@@ -0,0 +1,7 @@
+let link_switch = document.getElementById("link_switch");
+let arrow_down = document.getElementById("arrow_down");
+
+link_switch.addEventListener("click",function(e){
+    e.preventDefault();
+    
+});
