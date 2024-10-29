84c92b991cdf938f4c7cfbbea39490c251b6396d
CPC2-2 <cmtorreno@gmail.com>
2024-10-29 13:17:39 +0100

    updated js 29/10/2024

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,6 +1,14 @@
 let link_switch = document.getElementById("link_switch");
 let arrow_down = document.getElementById("arrow_down");
+let categori_name = document.querySelectorAll(".categori_name");
 
 link_switch.addEventListener("click",function(e){
     e.preventDefault();
+});
+
+categori_name.forEach(categori => { // cambio de imagen
+    categori.addEventListener("click", function(e) {
+        categori.style.transition = "background-image 100ms ease"
+        categori.style.backgroundImage = "url('/Assets/img/abajo.png')"; // Envuélvelo en url("")
+    });
 });
