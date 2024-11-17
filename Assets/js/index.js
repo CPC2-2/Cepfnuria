352cb079d5b55f139b4d8c8a295c1dd06160ffe8
CPC2-2 <cmtorreno@gmail.com>
2024-11-17 12:54:39 +0100

    updated 12:45

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,6 +1,8 @@
 let link_switch = document.getElementById("link_switch");
 let arrow_down = document.getElementById("arrow_down");
 let categori_name = document.querySelectorAll(".categori_name");
+let subcategoriesbox =  document.getElementById("subcategories_box");
+let subcategoriesboxall = document.querySelectorAll("subcategories_box");
 
 link_switch.addEventListener("click",function(e){
     e.preventDefault();
@@ -11,8 +13,10 @@ categori_name.forEach(categori => { // cambio de imagen
         categori.style.transition = "background-image 100ms ease";
         if (categori.style.backgroundImage === 'url("/Assets/img/abajo.png")') {
             categori.style.backgroundImage = 'url("/Assets/img/arriba.png")';
+            subcategoriesbox.style.display = "none";
         } else {
             categori.style.backgroundImage = 'url("/Assets/img/abajo.png")';
+            subcategoriesbox.style.display = "block";
         }
     });
 });
